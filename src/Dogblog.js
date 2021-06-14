1462e7e9ce10737c3114186d1facbf1bc7533716
TrentSisson <trent.c.sisson0@gmail.com>
2021-06-14 05:16:47 -0500

    got dogblog to render, fixed login, and made a skeleton spplicstion views from different app I need to change.

---
+++ src/Dogblog.js
@@ -0,0 +1,16 @@
+import React from "react"
+//redirect will take you to a designated path(when someone is not logged in they are redirected to the login)
+import { Route } from "react-router-dom"
+//import { Login } from "./auth/Login"
+//applicationview is what is rendered on a certain pathway
+import { ApplicationView } from "./components/AplicationViews.js"
+//import { Nav } from "./Nav/Nav"
+//import "./Dogblog.css"
+
+
+export const DogBlog = () => (
+  <>
+    {/* <Route render={props => <Nav {...props} />} /> */}
+    <Route render={props => <ApplicationView {...props} />} />
+  </>
+)
---
+++ src/components/AplicationViews.js
@@ -0,0 +1,13 @@
+import React from  "react"
+
+
+
+export const ApplicationView =(props) =>{
+    return (
+        <>
+        <div className = "Test">
+            DogBlog
+        </div>
+        </>
+    )
+}
