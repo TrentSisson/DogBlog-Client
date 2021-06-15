d60076f44fdde4a53844a69637c02dff112f44d1
TrentSisson <trent.c.sisson0@gmail.com>
2021-06-15 05:28:36 -0500

    created the nav bar files along with the Login access so that the Nav bar takes you to the login page, nav bar is 100% functional, Login still work in progress will be completed on a later branch

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,5 @@
 import React, { useRef } from "react"
 import { Link } from "react-router-dom";
-import "./Login.css"
 
 
 export const Login = props => {
@@ -10,7 +9,7 @@ export const Login = props => {
     const passwordDialog = useRef()
 
     const existingUserCheck = () => {
-        return fetch(`http://localhost:8088/customers?email=${email.current.value}`)
+        return fetch(`http://localhost:8088/users?email=${email.current.value}`)
             .then(_ => _.json())
             .then(user => user.length ? user[0] : false)
     }
